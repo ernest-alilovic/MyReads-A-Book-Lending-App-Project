8c9ea3b4d8f4bff6a40256779dc450f5ca5b3f29
Ernest Alilovic <ernest.alilovic@gmail.com>
2018-08-17 22:54:53 +0200

    FIX

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
-// import * as BooksAPI from './BooksAPI';
+import * as BooksAPI from './BooksAPI';
 import { Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import SearchPage from './SearchPage';
 import './App.css';
 
 class BooksApp extends React.Component {
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      this.setState({ books });
+    });
+  }
   render() {
     return (
       <div className="app">
-        <Route exact path='/' component={MainPage} />
+        <Route exact path='/' render={() =>
+          <MainPage books={this.state.books} />
+        } />
         <Route path='/search' component={SearchPage} />
       </div>
     )
