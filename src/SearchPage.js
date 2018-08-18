31a34e48c922a0b6d373f308742beaf7477a5646
Ernest Alilovic <ernest.alilovic@gmail.com>
2018-08-18 19:57:36 +0200

    Modified SearchPage.js

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
+import escapeRegExp from 'escape-string-regexp';
 
 class SearchPage extends Component {
+  state = {
+    query: ''
+  }
+
+  updateQuery = (query) => {
+    this.setState({ query })
+  }
+
   render() {
     return (
       <div className="search-books">
         <div className="search-books-bar">
           <Link className="close-search" to='/' alt='Close'>Close</Link>
           <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title or author"/>
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={this.state.query}
+              onChange={(event) => this.updateQuery(event.target.value)}
+            />
           </div>
         </div>
         <div className="search-books-results">
