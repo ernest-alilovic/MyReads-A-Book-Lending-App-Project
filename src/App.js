9cea2701de6c06c3d599725931c141b4ae3ab1d6
Ernest Alilovic <ernest.alilovic@gmail.com>
2018-08-18 18:23:43 +0200

    Added refresh feature to update logic

--- src/App.js
+++ src/App.js
@@ -18,6 +18,9 @@ class BooksApp extends React.Component {
 
   moveShelf = (book, shelf) => {
     BooksAPI.update(book, shelf);
+    BooksAPI.getAll().then(books => {
+      this.setState({ books });
+    });
   }
 
   render() {
