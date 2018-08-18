3bdcd3eac525ef1dc4f6dd71558f99ce12b0e4de
Ernest Alilovic <ernest.alilovic@gmail.com>
2018-08-18 17:24:26 +0200

    Displayed both pages

--- src/MainPage.js
+++ src/MainPage.js
@@ -4,10 +4,41 @@ import Book from './Book';
 
 class MainPage extends Component {
   render() {
+    const shelves = {
+      currentlyReading: 'Currently Reading',
+      wantToRead: 'Want To Read',
+      read: 'read'
+    };
     return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
         <div className="open-search">
           <Link to='/search' alt='Add a book'>Add a book</Link>
         </div>
+        {Object.keys(shelves).map((shelf) => {
+          return (
+            <div key={shelf} className="list-books-content">
+              <div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">{shelves[shelf]}</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      {this.props.books
+                        .filter(book => book.shelf === shelf)
+                        .map(book => (
+                          <Book key={book.title} book={book} />
+                        ))
+                      }
+                    </ol>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
     )
   }
 }
